ad2bdda075b0430a818af80e9b51fc2d187cd833
Sarah Gunnels Porter <zele.de.cuisine@gmail.com>
2020-07-19 15:27:08 -0400

    created load template
    look into changing segue to pass whole Pin, rather than pinId.

--- Virtual Tourist/Virtual Tourist/Controllers/PhotoAlbumViewController.swift
+++ Virtual Tourist/Virtual Tourist/Controllers/PhotoAlbumViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreData
 import MapKit
 
-class PhotoAlbumViewController: UIViewController, MKMapViewDelegate, UICollectionViewDelegate {
+class PhotoAlbumViewController: UIViewController, MKMapViewDelegate, UICollectionViewDelegate, NSFetchedResultsControllerDelegate {
 
     //MARK: View Outlets
     @IBOutlet weak var mapView: MKMapView!
@@ -18,7 +18,9 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate, UICollectio
     
     //MARK: Data Handling
     var dataContext:NSManagedObjectContext!
-    var pinId:String?
+    var pinId:String!
+    var fetchedPinResultsController:NSFetchedResultsController<Pin>!
+    var fetchedPhotoResultsController:NSFetchedResultsController<Photo>!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +29,52 @@ class PhotoAlbumViewController: UIViewController, MKMapViewDelegate, UICollectio
         self.photoCollectionView.delegate = self
         
         // Load the Pin
+        self.loadPhotoData()
+    }
+    
+    //MARK: Load the Pin & Photo Data
+    func loadPhotoData(){
+        // get get Pin with pinId given by MapView
+        let pinFetchRequest:NSFetchRequest<Pin> = Pin.fetchRequest()
+        let pinPredicate = NSPredicate(format: "id == %@", UUID(uuidString: pinId)! as CVarArg)
+        pinFetchRequest.predicate = pinPredicate
+        let sortDescriptor = NSSortDescriptor(key: "id", ascending: true)
+        pinFetchRequest.sortDescriptors = [sortDescriptor]
+        
+        fetchedPinResultsController = NSFetchedResultsController(fetchRequest: pinFetchRequest, managedObjectContext: dataContext, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedPinResultsController.delegate = self
+
+        do {
+            try fetchedPinResultsController.performFetch()
+        } catch {
+            fatalError("The fetch could not be performed: \(error.localizedDescription)")
+        }
+        // make fetch request for the photos for this Pin
+        let photoFetchRequest:NSFetchRequest<Photo> = Photo.fetchRequest()
+        let photoPredicate = NSPredicate(format: "pin == %@", fetchedPinResultsController.fetchedObjects!.first!)
+        photoFetchRequest.predicate = photoPredicate
+        fetchedPhotoResultsController = NSFetchedResultsController(fetchRequest: photoFetchRequest, managedObjectContext: dataContext, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedPhotoResultsController.delegate = self
+        do{
+            try fetchedPhotoResultsController.performFetch()
+        } catch {
+            fatalError("The fetch could not be performed: \(error.localizedDescription)")
+        }
+        // check if photos exist for this pin
+        if let photos = fetchedPhotoResultsController.fetchedObjects {
+            for photo in photos {
+                // set photos in the collection view
+            }
+        } else {
+            self.downloadPhotosFromFlickr()
+        }
+    }
+    
+    func downloadPhotosFromFlickr(){
+        //To Do: Download photos from Flickr
+        // download new set of photos for this pin from Flickr in background queue
+        // display in view in main queue as they download
+        // save new photos to Core Data as they download in background queue
     }
     
 
